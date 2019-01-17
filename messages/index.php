4169d632a0fa8c646957ac3822cbc250a6f834fc
follacom <mathieu.follaco@etud.u-picardie.fr>
2019-01-18 00:20:14 +0100

    send Post from message

--- messages/index.php
+++ messages/index.php
@@ -1,5 +1,10 @@
 <?php
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+
+require '../vendor/autoload.php';
+
 if (!empty($_POST['payload'])) {
     $payload = [];
 
@@ -7,17 +12,35 @@ if (!empty($_POST['payload'])) {
 
     $responsejson = [];
 
-    var_dump($payload);
-
     $inputs = $payload->inputs;
 
-    var_dump($inputs);
+    $client = new Client([
+        // Base URI is used with relative requests
+        'base_uri' => 'http://nightcode-phobos.cleverapps.io/input/',
+        // You can set any number of default request options.
+        'timeout' => 2.0,
+    ]);
 
-    foreach ($inputs as $input)
-        $response[] = [
+    foreach ($inputs as $input) {
+        $response = [
             "external_id" => $input->uuid,
             "content" => $input->msg
         ];
+
+        $response = json_encode($response);
+
+        $request = new Request('POST', 'messages',
+            [
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Content-Type' => 'application/json',
+                    'X-API-Key' => 'f5849aa8e9a7b4df436902587209058011484473a0c66c0db0440985671a2589'
+                ]
+            ],$response);
+
+        $responsePost = $client->send($request);
+        echo $responsePost->getStatusCode();
+    }
 
     $responsejson = json_encode($response);
 
