b69e3a4a5b753d321f956bf3d7950df1d84f3042
LDeschamps <leo.deschamps@etud.u-picardie.fr>
2019-01-18 03:58:16 +0100

    modif index pour ko science-datas

--- science-datas/index.php
+++ science-datas/index.php
@@ -7,84 +7,100 @@ use GuzzleHttp\Exception\RequestException;
 
 require '../vendor/autoload.php';
 
-if (!empty(file_get_contents("php://input"))) {
+$php_input = file_get_contents("php://input");
+if (isset($php_input)) {
     $request = [];
 
     $request = json_decode(file_get_contents("php://input"));
 
-    $uuid = $request->uuid;
+    var_dump($request);
 
-    $datas = $request->datas;
+    if(!empty($request->uuid) && !empty($request->datas)){
 
-    $client = new Client([
-        // Base URI is used with relative requests
-        'base_uri' => 'http://nightcode-phobos.cleverapps.io/input/science-datas/',
-        // You can set any number of default request options.
-        'timeout' => 2.0,
-    ]);
+        $uuid = $request->uuid;
+        $datas = $request->datas;
 
-    foreach ($datas as $data) {
-        switch($data->unit){
-                case "celsius-degrees":
-                        $donnees[] = [
-                            "type" => $data->type,
-                            "unit" => "fahrenheit-degrees",
-                            "value" => ($data->value*9/5)+32
-                        ];                        
-                        break;
-                case "milli-pirate-ninjas":   
-                        $donnees[] = [
-                            "type" => $data->type,
-                            "unit" => "kilo-joules",
-                            "value" => $data->value
-                        ];                     
-                        break;
-                case "pounds":
-                        $donnees[] = [
-                            "type" => $data->type,
-                            "unit" => "kilograms",
-                            "value" => $data->value/2.205
-                        ]; 
-                        break;
-                case "gallons":
-                        $donnees[] = [
-                            "type" => $data->type,
-                            "unit" => "liters",
-                            "value" => $data->value*3.785
-                        ]; 
-                        break;
-                default:
-                        break;
+        foreach($datas as $data){
+            if(!empty($data->type) && !empty($data->unit) && !empty($data->value)){
+
+                $client = new Client([
+                // Base URI is used with relative requests
+                'base_uri' => 'http://nightcode-phobos.cleverapps.io/input/science-datas/',
+                // You can set any number of default request options.
+                'timeout' => 2.0,
+                ]);
+
+                foreach ($datas as $data) {
+                    switch($data->unit){
+                            case "celsius-degrees":
+                                    $donnees[] = [
+                                        "type" => $data->type,
+                                        "unit" => "fahrenheit-degrees",
+                                        "value" => ($data->value*9/5)+32
+                                    ];
+                                    break;
+                            case "milli-pirate-ninjas":
+                                    $donnees[] = [
+                                        "type" => $data->type,
+                                        "unit" => "kilo-joules",
+                                        "value" => $data->value
+                                    ];
+                                    break;
+                            case "pounds":
+                                    $donnees[] = [
+                                        "type" => $data->type,
+                                        "unit" => "kilograms",
+                                        "value" => $data->value/2.205
+                                    ];
+                                    break;
+                            case "gallons":
+                                    $donnees[] = [
+                                        "type" => $data->type,
+                                        "unit" => "liters",
+                                        "value" => $data->value*3.785
+                                    ];
+                                    break;
+                            default:
+                                    break;
+                    }
+                }
+
+                $response = [
+                                "external_id" => $uuid,
+                                "datas" => $donnees
+                            ];
+
+                $response = json_encode($response);
+
+                $request = new Request('POST', 'add',
+                    [
+                            'Accept' => 'application/json',
+                            'Content-Type' => 'application/json',
+                            'x-api-key' => 'f5849aa8e9a7b4df436902587209058011484473a0c66c0db0440985671a2589'
+
+                    ],$response);
+
+                try {
+                    $responsePost = $client->send($request);
+                } catch (RequestException $e) {
+                    echo Psr7\str($e->getRequest());
+                    if ($e->hasResponse()) {
+                        echo Psr7\str($e->getResponse());
+                    }
+                }
+
+                $responsejson = json_encode($response);
+            }
+            else {
+                header("HTTP/1.1 400 Bad Request");
+                return false;
+            }
         }
-        
-        
+        return false;
     }
-
-    $response = [
-                    "external_id" => $uuid,
-                    "datas" => $donnees
-                ];
-
-    $response = json_encode($response);
-
-    $request = new Request('POST', 'add',
-        [
-                'Accept' => 'application/json',
-                'Content-Type' => 'application/json',
-                'x-api-key' => 'f5849aa8e9a7b4df436902587209058011484473a0c66c0db0440985671a2589'
-
-        ],$response);
-
-    try {
-        $responsePost = $client->send($request);
-    } catch (RequestException $e) {
-        echo Psr7\str($e->getRequest());
-        if ($e->hasResponse()) {
-            echo Psr7\str($e->getResponse());
-        }
+    else {
+        //header("HTTP/1.1 400 Bad Request");
     }
-        
-    $responsejson = json_encode($response);
-
-    echo $responsejson;
 }
+
+    
