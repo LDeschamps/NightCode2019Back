5a99a6d8b75d9967cd8565bb79a265a5ab774b13
follacom <mathieu.follaco@etud.u-picardie.fr>
2019-01-18 05:23:32 +0100

    je sais pas

--- messages/index.php
+++ messages/index.php
@@ -2,7 +2,6 @@
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
-use GuzzleHttp\Psr7;
 use GuzzleHttp\Exception\RequestException;
 
 require '../vendor/autoload.php';
@@ -11,93 +10,135 @@ $request = [];
 
 $request = json_decode(file_get_contents("php://input"));
 
-$payload = $request->payload;
-
-$inputs = $payload->inputs;
-
-$client = new Client([
-    // Base URI is used with relative requests
-    'base_uri' => 'http://nightcode-phobos.cleverapps.io/input/',
-    // You can set any number of default request options.
-    'timeout' => 8.0,
-]);
-
-if (empty($inputs)){
-    header("HTTP/1.1 400 Bad Request");
-    exit();
+if (!empty($request->body)) {
+    $request = $request->body;
 }
-foreach ($inputs as $input) {
-    if (!empty($input->msg)) {
-        $response = [
-            "external_id" => $input->uuid,
-            "content" => $input->msg
-        ];
-        $response = json_encode($response);
 
-        $request = new Request('POST', 'messages',
-            [
-                'Accept' => 'application/json',
-                'Content-Type' => 'application/json',
-                'x-api-key' => 'f5849aa8e9a7b4df436902587209058011484473a0c66c0db0440985671a2589'
-
-            ], $response);
-
-        try {
-            $responsePost = $client->send($request);
-        } catch (RequestException $e) {
-            echo Psr7\str($e->getRequest());
-            if ($e->hasResponse()) {
-                echo Psr7\str($e->getResponse());
+if (!empty($request->type)) {
+    $type = $request->type;
+    switch ($type) {
+        case "default":
+            $payload = $request->payload;
+            $inputs = $payload->inputs;
+            if (empty($inputs)) {
+                header("HTTP/1.1 400 Bad Request");
+                exit();
             }
-        }
-    } elseif (!empty($input->text)) {
-        $response = [
-            "external_id" => $input->uuid,
-            "message" => $input->text
-        ];
-        $response = json_encode($response);
+            $baseuri = "http://nightcode-phobos.cleverapps.io/input/messages";
+            foreach ($inputs as $input) {
+                if (!empty($input->msg)) {
+                    $response = [
+                        "external_id" => $input->uuid,
+                        "content" => $input->msg
+                    ];
 
-        $request = new Request('POST', 'moms-messages/add',
-            [
-                'Accept' => 'application/json',
-                'Content-Type' => 'application/json',
-                'x-api-key' => 'f5849aa8e9a7b4df436902587209058011484473a0c66c0db0440985671a2589'
+                    $client = new Client([
+                        'base_uri' => $baseuri,
+                    ]);
+                    $response = json_encode($response);
 
-            ], $response);
+                    $request = new Request('POST', '',
+                        [
+                            'Accept' => 'application/json',
+                            'Content-Type' => 'application/json',
+                            'x-api-key' => 'f5849aa8e9a7b4df436902587209058011484473a0c66c0db0440985671a2589'
 
-        try {
-            $responsePost = $client->send($request);
-        } catch (RequestException $e) {
-            echo Psr7\str($e->getRequest());
-            if ($e->hasResponse()) {
-                echo Psr7\str($e->getResponse());
+                        ], $response);
+
+                    try {
+                        $responsePost = $client->send($request);
+                    } catch (RequestException $e) {
+                        header("HTTP/1.1 400 Bad Request");
+                        exit();
+                    }
+                } else {
+                    header("HTTP/1.1 400 Bad Request");
+                    exit();
+                }
             }
-        }
-    } elseif (!empty($input->content)){
-        $response = [
-            "external_id" => $input->uuid,
-            "message" => $input->content
-        ];
-        $response = json_encode($response);
-
-        $request = new Request('POST', 'clerentins-messages/add',
-            [
-                'Accept' => 'application/json',
-                'Content-Type' => 'application/json',
-                'x-api-key' => 'f5849aa8e9a7b4df436902587209058011484473a0c66c0db0440985671a2589'
-
-            ], $response);
-
-        try {
-            $responsePost = $client->send($request);
-        } catch (RequestException $e) {
-            echo Psr7\str($e->getRequest());
-            if ($e->hasResponse()) {
-                echo Psr7\str($e->getResponse());
+            break;
+        case "message-to-mom":
+            $payload = $request->payload;
+            $messages = $payload->messages;
+            if (empty($messages)) {
+                header("HTTP/1.1 400 Bad Request");
+                exit();
             }
-        }
-    }
-    else{
-        header("HTTP/1.1 400 Bad Request");
+            $baseuri = "http://nightcode-phobos.cleverapps.io/input/moms-messages/add";
+            foreach ($messages as $message) {
+                if (!empty($message->text)) {
+                    $response = [
+                        "external_id" => $message->uuid,
+                        "message" => $message->text
+                    ];
+
+                    $client = new Client([
+                        'base_uri' => $baseuri,
+                    ]);
+                    $response = json_encode($response);
+
+                    $request = new Request('POST', '',
+                        [
+                            'Accept' => 'application/json',
+                            'Content-Type' => 'application/json',
+                            'x-api-key' => 'f5849aa8e9a7b4df436902587209058011484473a0c66c0db0440985671a2589'
+
+                        ], $response);
+
+                    try {
+                        $responsePost = $client->send($request);
+                    } catch (RequestException $e) {
+                        header("HTTP/1.1 400 Bad Request");
+                        exit();
+                    }
+                } else {
+                    header("HTTP/1.1 400 Bad Request");
+                    exit();
+                }
+            }
+            break;
+        case "for-mr-clerentin":
+            $messages = $request->messages;
+            if (empty($messages)) {
+                header("HTTP/1.1 400 Bad Request");
+                exit();
+            }
+            $baseuri = "http://nightcode-phobos.cleverapps.io/input/clerentins-messages/add";
+            foreach ($messages as $message) {
+                if (!empty($message->text)) {
+                    $response = [
+                        "external_id" => $message->uuid,
+                        "message" => $message->text
+                    ];
+
+                    $client = new Client([
+                        'base_uri' => $baseuri,
+                    ]);
+                    $response = json_encode($response);
+
+                    $request = new Request('POST', '',
+                        [
+                            'Accept' => 'application/json',
+                            'Content-Type' => 'application/json',
+                            'x-api-key' => 'f5849aa8e9a7b4df436902587209058011484473a0c66c0db0440985671a2589'
+
+                        ], $response);
+
+                    try {
+                        $responsePost = $client->send($request);
+                    } catch (RequestException $e) {
+                        header("HTTP/1.1 400 Bad Request");
+                        exit();
+                    }
+                } else {
+                    header("HTTP/1.1 400 Bad Request");
+                    exit();
+                }
+            }
+            break;
+        default:
+            header("HTTP/1.1 400 Bad Request");
+            exit();
+            break;
     }
 }
