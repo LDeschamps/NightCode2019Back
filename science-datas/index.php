be4ad3a42c970b069e114d5895cd31fc557d2854
LDeschamps <leo.deschamps@etud.u-picardie.fr>
2019-01-18 05:48:40 +0100

    modif science datas index.php

--- science-datas/index.php
+++ science-datas/index.php
@@ -15,36 +15,36 @@ $client = new Client([
 foreach($datas as $data){
     if(!empty($data->type) && !empty($data->unit) && !empty($data->value) && !empty($request->uuid) && !empty($request->datas)){
         switch($data->unit){
-                case "celsius-degrees":
-                        $donnees[] = [
-                            "type" => $data->type,
-                            "unit" => "fahrenheit-degrees",
-                            "value" => ($data->value*9/5)+32
-                        ];
-                        break;
-                case "milli-pirate-ninjas":
-                        $donnees[] = [
-                            "type" => $data->type,
-                            "unit" => "kilo-joules",
-                            "value" => $data->value
-                        ];
-                        break;
-                case "pounds":
-                        $donnees[] = [
-                            "type" => $data->type,
-                            "unit" => "kilograms",
-                            "value" => $data->value/2.205
-                        ];
-                        break;
-                case "gallons":
-                        $donnees[] = [
-                            "type" => $data->type,
-                            "unit" => "liters",
-                            "value" => $data->value*3.785
-                        ];
-                        break;
-                default:
-                        break;
+            case "celsius-degrees":
+                    $donnees[] = [
+                        "type" => $data->type,
+                        "unit" => "fahrenheit-degrees",
+                        "value" => ($data->value*9/5)+32
+                    ];
+                    break;
+            case "milli-pirate-ninjas":
+                    $donnees[] = [
+                        "type" => $data->type,
+                        "unit" => "kilo-joules",
+                        "value" => $data->value
+                    ];
+                    break;
+            case "pounds":
+                    $donnees[] = [
+                        "type" => $data->type,
+                        "unit" => "kilograms",
+                        "value" => $data->value/2.205
+                    ];
+                    break;
+            case "gallons":
+                    $donnees[] = [
+                        "type" => $data->type,
+                        "unit" => "liters",
+                        "value" => $data->value*3.785
+                    ];
+                    break;
+            default:
+                    break;
         }
     }
     else {
@@ -56,18 +56,19 @@ $response = [
                 "external_id" => $uuid,
                 "datas" => $donnees
             ];
-        $responsejson = json_encode($response);
-        $request = new Request('POST', 'add',
-            [
-                    'Accept' => 'application/json',
-                    'Content-Type' => 'application/json',
-                    'x-api-key' => 'f5849aa8e9a7b4df436902587209058011484473a0c66c0db0440985671a2589'
-            ],$responsejson);
-        try {
-            $responsePost = $client->send($request);
-        } catch (RequestException $e) {
-            echo Psr7\str($e->getRequest());
-            if ($e->hasResponse()) {
-                echo Psr7\str($e->getResponse());
-            }
-        }
+$responsejson = json_encode($response);
+$request = new Request('POST', 'add',
+    [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+            'x-api-key' => 'f5849aa8e9a7b4df436902587209058011484473a0c66c0db0440985671a2589'
+    ],$responsejson);
+try {
+    $responsePost = $client->send($request);
+} catch (RequestException $e) {
+    echo Psr7\str($e->getRequest());
+    if ($e->hasResponse()) {
+        echo Psr7\str($e->getResponse());
+    }
+}
+var_dump($responsejson);
